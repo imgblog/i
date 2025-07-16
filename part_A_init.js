c275f3f1d5a1cd1306b1d111943672c7b22e0e84
xiutk <huongtamky2017@gmail.com>
2025-07-16 10:48:23 +0700

    Add files via upload

---
+++ part_A_init.js
@@ -0,0 +1,6 @@
+const canvas = document.querySelector('#jansharecanvas');
+const canvas2 = document.querySelector('#jansharecanvas2');
+var imageDataByteLen = document.querySelector('#imagedata-byte-length');
+var bufferByteLen = document.querySelector('#arraybuffer-byte-length');
+var mimeType = 'image/png';
+var tenanhxuat='img';
