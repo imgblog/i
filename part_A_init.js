1f580eca4976326d981d2a56c26ef77ad3997324
imgblog <huongtamky2017@gmail.com>
2025-07-31 22:24:10 +0700

    Delete part_A_init.js

--- part_A_init.js
+++
@@ -1,6 +0,0 @@
-const canvas = document.querySelector('#jansharecanvas');
-const canvas2 = document.querySelector('#jansharecanvas2');
-var imageDataByteLen = document.querySelector('#imagedata-byte-length');
-var bufferByteLen = document.querySelector('#arraybuffer-byte-length');
-var mimeType = 'image/png';
-var tenanhxuat='img';
